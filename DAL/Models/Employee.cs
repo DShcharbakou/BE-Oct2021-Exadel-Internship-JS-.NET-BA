522a6b17f7cd5ffa297f9605113849166db75ef2
Темирбеков Арслан <arslan051099@gmail.com>
2021-10-15 19:24:02 +0200

    emails to email

--- DAL/Models/Employee.cs
+++ DAL/Models/Employee.cs
@@ -11,7 +11,7 @@ namespace DAL.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public string Emails { get; set; }
+        public string Email { get; set; }
         public string Phone { get; set; }
         public string Skype { get; set; }
         public int RoleId { get; set; }

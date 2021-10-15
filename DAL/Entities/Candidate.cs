ff0b791edc5f5acbacd024ee983be436d7a95342
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-10-15 13:04:41 +0300

    Add entity Candidate

---
+++ DAL/Entities/Candidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DAL
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Skype { get; set; }
+        public string Specialisation { get; set; }
+        public string Location { get; set; }
+        public string EnglishLevel { get; set; }
+        public DateTime RegDate { get; set; }
+        public bool IsArchived { get; set; }
+    }
+}

37ad76a1b03c7148d3ad7bb65b73970af02cb32c
CatholicCardinal <79603714+CatholicCardinal@users.noreply.github.com>
2021-10-15 16:13:21 +0300

    Hr add

---
+++ DAL/Models/HrInterviews.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DAL
+{
+    public class HrInterviews
+    {
+        public int ID { get; set; }
+        public DateTime Data { get; set; }
+        public int ScaleScore { get; set; }
+        public string FeedBack { get; set; }
+        public int EnglishLevel { get; set; }
+
+        //public List<Candidates> CandidateId 
+        //public List<Employees> EmployeeId 
+    }
+}

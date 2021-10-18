1c41069fd43c4b0df2534eaba0c5d8f41dbc52e9
AnnKirienko <anna.kirienko.st@gmail.com>
2021-10-18 16:00:47 +0200

    Add TopicStack DB class

---
+++ DAL/Models/TopicStack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class TopicStack
+    {
+        public int TopicID {get; set;}
+        public int StackID {get; set;}
+    }
+}

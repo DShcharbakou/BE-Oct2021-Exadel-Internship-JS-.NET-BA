886e8f69292154741a5532dad3b27299a9e4d7d1
JOKER <sasha.golubovich@tut.by>
2021-10-19 13:37:43 +0300

    Add dbModel BaseModel.cs

--- DAL/Repositories/BaseRepository.cs
+++ DAL/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DAL.Repositories
 {
-    public abstract class BaseRepository
+    public abstract class BaseRepository<T> where T : BaseModel
     {
     }
 }

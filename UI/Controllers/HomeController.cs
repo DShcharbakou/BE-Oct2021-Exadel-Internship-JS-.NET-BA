6460eb297f03052c30265d5091244abf72b2c6b5
Темирбеков Арслан <arslan051099@gmail.com>
2021-10-21 17:42:11 +0200

    Adding homeController,open swager without IISExpress

---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+    }
+}

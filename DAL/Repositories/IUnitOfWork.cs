bdab620860745f4357d75ed5deab02a93aa9c57f
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-10-24 16:38:26 +0300

    Added interface IUnitOfWork

---
+++ DAL/Repositories/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+namespace DAL.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        
+        BaseRepository<Candidate> Candidates { get; }
+        BaseRepository<Employee> Employees { get; }
+        BaseRepository<InternshipTeam> InternshipTeams { get; }
+        BaseRepository<Interview> Interviews { get; }
+        BaseRepository<Stack> Stacks { get; }
+        BaseRepository<Topic> Topics { get; }
+        void Save();
+    }
+}

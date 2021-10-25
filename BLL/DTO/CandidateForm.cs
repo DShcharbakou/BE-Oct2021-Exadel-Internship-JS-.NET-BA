29f0e30602022f34caf5b707d298ce0d767e4221
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-10-25 16:36:35 +0300

    deleted CandidateDTO and Add CandidateForm

--- BLL/DTO/CandidateForm.cs
+++ BLL/DTO/CandidateForm.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace BLL.DTO
 {
-    public class CandidateDTO
+    public class CandidateForm
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Skype { get; set; }
         public string Specialization { get; set; }
         public string Location { get; set; }
         public string EnglishLevel { get; set; }

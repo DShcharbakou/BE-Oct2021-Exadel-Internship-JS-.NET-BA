988eedacd287daafdff5858ca0995b662a8b44bb
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-10-26 15:37:52 +0300

    Created EmployeeDTO.cs,  IEmployeeService.cs and EmployeeService.cs

---
+++ BLL/DTO/EmployeeDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Skype { get; set; }
+        public int RoleId { get; set; }
+
+        // public ICollection<Interview> Interviews { get; set; }
+        // public ICollection<EmployeeStack> EmployeeStack { get; set; }
+        //public ICollection<TeamMentor> TeamMentors { get; set; }
+    }
+}

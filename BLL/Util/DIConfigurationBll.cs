9afc7366f5e199f8e86a888c56f58e15852d5daf
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-10-26 15:43:52 +0300

    Created DIConfigurationDal and DIConfigurationBll

---
+++ BLL/Util/DIConfigurationBll.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Util;
+using DAL.Repositories;
+using BLL.Interfaces;
+using BLL.Services;
+
+namespace BLL.Util
+{
+    public class DIConfigurationBll
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // DI need to know about AddTransient, AddSingleton, AddScoped
+            services.AddTransient<ICandidateService, CandidateService>();
+            services.AddTransient<IEmployeeService, EmployeeService>();
+            DIConfigurationDal.ConfigureServices(services);
+        }
+    }
+}

5d2e2c22bea95013cc731f8bc9ddf66b76f752c4
CatholicCardinal <79603714+CatholicCardinal@users.noreply.github.com>
2021-11-10 10:17:07 +0300

    CloseIssuesPart3
    Identical repositories. А lot of repetitive code #34

---
+++ DAL/Repositories/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Models;
+using System.Collections.Generic;
+
+namespace DAL.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseModel
+    {
+        List<T> GetAll();
+    }
+}

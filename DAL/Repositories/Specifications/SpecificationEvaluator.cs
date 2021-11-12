f4c5b78b0a807bab42215fa1d5a980db3c8e75a8
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-12 15:55:18 +0300

    Added SpecificationEvaluator.cs

---
+++ DAL/Repositories/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,51 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Specifications
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseModel
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> query, IBaseSpecifications<TEntity> specifications)
+        {
+            // Do not apply anything if specifications is null
+            if (specifications == null)
+            {
+                return query;
+            }
+
+            // Modify the IQueryable
+            // Apply filter conditions
+            if (specifications.FilterCondition != null)
+            {
+                query = query.Where(specifications.FilterCondition);
+            }
+
+            // Includes
+            query = specifications.Includes
+                        .Aggregate(query, (current, include) => current.Include(include));
+
+            // Apply ordering
+            if (specifications.OrderBy != null)
+            {
+                query = query.OrderBy(specifications.OrderBy);
+            }
+            else if (specifications.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(specifications.OrderByDescending);
+            }
+
+            // Apply GroupBy
+            if (specifications.GroupBy != null)
+            {
+                query = query.GroupBy(specifications.GroupBy).SelectMany(x => x);
+            }
+
+            return query;
+        }
+    }
+}

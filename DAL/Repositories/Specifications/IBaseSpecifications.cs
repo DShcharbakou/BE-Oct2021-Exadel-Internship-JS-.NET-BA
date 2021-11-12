866643368417bf8cb6392aa302ebd987761068d4
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-12 15:53:22 +0300

    Added IBaseSpecifications

---
+++ DAL/Repositories/Specifications/IBaseSpecifications.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Specifications
+{
+    public interface IBaseSpecifications<T>
+    {
+        Expression<Func<T, bool>> FilterCondition { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> GroupBy { get; }
+    }
+}

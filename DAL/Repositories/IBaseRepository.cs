5f3dd3dd6b7a8199549a1e724cef8d594c89a58a
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-15 14:59:23 +0300

    Updated IBaseRepository

--- DAL/Repositories/IBaseRepository.cs
+++ DAL/Repositories/IBaseRepository.cs
@@ -7,7 +7,8 @@ namespace DAL.Repositories
     public interface IBaseRepository<T> where T : BaseModel
     {
         List<T> GetAll();
-        T Get(int id, IBaseSpecifications<T> baseSpecifications = null);
+        T Get(int id);
+        IEnumerable<T> FindWithSpecificationPattern(IBaseSpecifications<T> baseSpecifications = null);
         void Save(T model);
         void Remove(T model);
         void Remove(int id);

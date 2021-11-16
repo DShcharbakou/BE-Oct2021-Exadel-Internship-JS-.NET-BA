935e15c8fb7af6f7c9704d75e23235b2cb0e1582
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-16 17:53:38 +0300

    Updated BaseSpecifications.cs

--- DAL/Repositories/Specifications/BaseSpecifications.cs
+++ DAL/Repositories/Specifications/BaseSpecifications.cs
@@ -9,19 +9,46 @@ namespace DAL.Repositories.Specifications
 {
     public class BaseSpecifications<T> : IBaseSpecifications<T>
     {
-        public BaseSpecifications(){}
+        private readonly List<Expression<Func<T, object>>> _includeCollection = new List<Expression<Func<T, object>>>();
 
-        public BaseSpecifications(Expression<Func<T, bool>> criteria)
+        public BaseSpecifications() { }
+        public BaseSpecifications(Expression<Func<T, bool>> filterCondition)
         {
-            Criteria = criteria;
+            this.FilterCondition = filterCondition;
         }
 
-        public Expression<Func<T, bool>> Criteria { get; }
-        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public Expression<Func<T, bool>> FilterCondition { get; private set; }
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+        public List<Expression<Func<T, object>>> Includes
+        {
+            get
+            {
+                return _includeCollection;
+            }
+        }
+
+        public Expression<Func<T, object>> GroupBy { get; private set; }
 
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
         }
+
+        protected void ApplyOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void ApplyOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
+        {
+            OrderByDescending = orderByDescendingExpression;
+        }
+
+        protected void SetFilterCondition(Expression<Func<T, bool>> filterExpression)
+        {
+            FilterCondition = filterExpression;
+        }
     }
 }
+

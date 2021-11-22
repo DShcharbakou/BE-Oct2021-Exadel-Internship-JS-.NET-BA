f44308a0ac0f66577a8812a54cae658a1a8b4fa2
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-22 19:29:39 +0300

    Create ISandboxService.cs

---
+++ BLL/Interfaces/ISandboxService.cs
@@ -0,0 +1,14 @@
+﻿using BLL.DTO;
+using System.Collections.Generic;
+
+namespace BLL.Interfaces
+{
+    public interface ISandboxService
+    {
+        IEnumerable<SandboxDTO> GetAllSandboxes();
+        SandboxDTO GetSandboxById(int id);
+        void AddSandbox(SandboxDTO sandboxDto);
+        void DeleteSandbox(int id);
+        int GetCurrentSandboxId();
+    }
+}

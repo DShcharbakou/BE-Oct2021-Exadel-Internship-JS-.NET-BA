5589fd8096bc0880702cb3fcded34ccb88ff2ea6
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-23 10:28:05 +0300

    updating specification

--- DAL/Repositories/Specifications/CandidatesForMentorSpecification.cs
+++ DAL/Repositories/Specifications/CandidatesForMentorSpecification.cs
@@ -6,10 +6,10 @@ namespace DAL.Repositories.Specifications
 {
     public class CandidatesForMentorSpecification : BaseSpecifications<Candidate>
     {
-        public CandidatesForMentorSpecification(int teamId, int currentSandbox) : base()
+        public CandidatesForMentorSpecification(int teamId) : base()
         {
             AddInclude(x => x.CandidateSandboxes);
-            SetFilterCondition(x => x.CandidateSandboxes.Where(y => y.SandboxID == currentSandbox).Select(x => x.Id).Contains(x.Id));
+            SetFilterCondition(x => x.CandidateSandboxes.Where(y => y.Sandbox.StartDate <= DateTimeOffset.UtcNow && y.Sandbox.EndDate >= DateTimeOffset.UtcNow).Select(x => x.Id).Contains(x.Id));
             SetFilterCondition(x => x.CandidateSandboxes.Where(y => y.SandboxTeams.Where(z => z.TeamID == teamId).Select(z => z.CandidateSandboxID).Contains(y.Id)).Select(w => w.CandidateID).Contains(x.Id));
         }
     }

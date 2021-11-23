2c5c5d277fd6a88293c6f18286d2e30b632697e7
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-23 22:02:46 +0300

    Adding services

---
+++ BLL/Services/CityService.cs
@@ -0,0 +1,29 @@
+﻿using BLL.Interfaces;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class CityService : ICityService
+    {
+        private protected IUnitOfWork _db;
+
+        public CityService(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public string GetCityById(int id)
+        {
+            var city = _db.Cities.Get(id).ToString();
+            var countryId = _db.Cities.Get(id).CountryID;
+            var country = _db.Countries.Get(countryId).ToString();
+            string location = $"{city}, {country}";
+            return location;
+        }
+    }
+}
---
+++ BLL/Services/SpecializationService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class SpecializationService : ISpecializationService
+    {
+        private readonly IUnitOfWork _db;
+
+        public SpecializationService(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public string GetSpecializationById(int id)
+        {
+            return _db.Specializations.Get(id).Name.ToString();
+        }
+    }
+}

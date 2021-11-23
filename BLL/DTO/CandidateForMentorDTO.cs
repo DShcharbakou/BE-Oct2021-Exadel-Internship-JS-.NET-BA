f2a745b737e90694e68c3b450f00460b2f0dbeb5
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-23 22:01:36 +0300

    Adding DTOs

---
+++ BLL/DTO/CandidateForMentorDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class CandidateForMentorDTO
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Skype { get; set; }
+        public string Specialization { get; set; }
+        public string Location { get; set; }
+        public string EnglishLevel { get; set; }
+    }
+}
---
+++ BLL/DTO/CityDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class CityDTO
+    {
+        public string City { get; set; }
+        public int Country{ get; set; }
+    }
+}

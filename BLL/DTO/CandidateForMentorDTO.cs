352fbc7651d07b26d9b56b07f33a2b37d540cef3
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-11-25 15:02:27 +0300

    Updated DTO

--- BLL/DTO/CandidateForMentorDTO.cs
+++ BLL/DTO/CandidateForMentorDTO.cs
@@ -8,14 +8,8 @@ using System.Threading.Tasks;
 
 namespace BLL.DTO
 {
-    public class CandidateForMentorDTO
+    public class CandidateForMentorDTO: CandidateDTO
     {
-        public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Skype { get; set; }
         public string Specialization { get; set; }
         public string Location { get; set; }
         public string EnglishLevel { get; set; }

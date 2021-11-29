ea81965f7fffc4fd665aae28a5179a19ade3ab4f
AnnKirienko <anna.kirienko.st@gmail.com>
2021-11-29 14:20:04 +0100

    Add new controller and service

---
+++ BLL/Services/GetUserIDService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using DAL;
+using DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+  public class GetUserIDService
+    {
+        private readonly IUnitOfWork _db;
+        private readonly IMapper _mapper;
+        public GetUserIDService(IUnitOfWork db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public EmployeeDTO GetEmployeeByEmail(string email)
+        {
+            return _mapper.Map<Employee, EmployeeDTO>(_db.Employees.GetAll().Where(x => x.Email == email).FirstOrDefault());
+        }
+    }
+}
---
+++ UI/Controllers/BaseController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using BLL.Interfaces;
+using DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly ICandidateService _candidateService;
+        private readonly IEmployeeService _employeeService;
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+        public BaseController(ICandidateService candidateService,
+                                IEmployeeService employeeService,
+                                IMapper mapper,
+                                UserManager<User> userManager)
+        {
+            _candidateService = candidateService;
+            _userManager = userManager;
+            _employeeService = employeeService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("candidates")]
+        [Authorize(Roles = "admin, mentor")]
+        public async Task<IEnumerable<CandidateDTO>> GetCandidates()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var employee = _employeeService.GetEmployeeByEmail(user.Email);
+            IEnumerable<CandidateDTO> candidates = _candidateService.GetCandidatesFromTeam(currentTeam.TeamNumber);
+            return candidates.ToList();
+        }
+    }
+}

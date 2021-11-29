8346f66a7f67ca541713a595feee70117fd46f96
JOKER <sasha.golubovich@tut.by>
2021-11-29 23:20:50 +0300

    Edit DirectoryController: add endpoints

--- UI/Controllers/DirectoryController.cs
+++ UI/Controllers/DirectoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.DTO;
+using BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,106 @@ namespace UI.Controllers
     [ApiController]
     public class DirectoryController : Controller
     {
-        public DirectoryController()
+        private readonly IDirectoryService _directoryService;
+        public DirectoryController(IDirectoryService directoryService)
         {
-
+            _directoryService = directoryService;
         }
 
-
+        [HttpGet("GetSpecializationById/{id}")]
         public SpecializationDTO GetSpecializationById(int specializationId)
         {
-            return 
+            return _directoryService.GetSpecializationById(specializationId);
         }
+
+        [HttpGet("GetAllSpecializations")]
+        public List<SpecializationDTO> GetAllSpecializations()
+        {
+            return _directoryService.GetAllSpecializations();
+        }
+
+        [HttpGet("GetEnglishLevelById/{id}")]
+        public EnglishLevelDTO GetEnglishLevelById(int englishLevelId)
+        {
+            return _directoryService.GetEnglishLevelById(englishLevelId);
+        }
+
+        [HttpGet("GetAllEnglishLevels")]
+        public List<EnglishLevelDTO> GetAllEnglishLevels()
+        {
+            return _directoryService.GetAllEnglishLevels();
+        }
+
+        [HttpGet("GetCityById/{id}")]
+        public CityDTO GetCityById(int cityId)
+        {
+            return _directoryService.GetCityById(cityId);
+        }
+
+        [HttpGet("GetAllCities")]
+        public List<CityDTO> GetAllCities()
+        {
+            return _directoryService.GetAllCities();
+        }
+
+        [HttpGet("GetCountryById/{id}")]
+        public CountryDTO GetCountryById(int countryId)
+        {
+            return _directoryService.GetCountryById(countryId);
+        }
+
+        [HttpGet("GetAllCountries")]
+        public List<CountryDTO> GetAllCountries()
+        {
+            return _directoryService.GetAllCountries();
+        }
+
+        [HttpGet("GetSkillById/{id}")]
+        public SkillDirectoryDTO GetSkillById(int skillId)
+        {
+            return _directoryService.GetSkillById(skillId);
+        }
+
+        [HttpGet("GetAllSkills")]
+        public List<SkillDirectoryDTO> GetAllSkills()
+        {
+            return _directoryService.GetAllSkills();
+        }
+
+        [HttpGet("GetTopicById/{id}")]
+        public TopicDTO GetTopicById(int topicId)
+        {
+            return _directoryService.GetTopicById(topicId);
+        }
+
+        [HttpGet("GetAllTopics")]
+        public List<TopicDTO> GetAllTopics()
+        {
+            return _directoryService.GetAllTopics();
+        }
+
+        [HttpGet("GetStateById/{id}")]
+        public StateDTO GetStateById(int stateId)
+        {
+            return _directoryService.GetStateById(stateId);
+        }
+
+        [HttpGet("GetAllStates")]
+        public List<StateDTO> GetAllStates()
+        {
+            return _directoryService.GetAllStates();
+        }
+
+        //[HttpGet("GetStatusById/{id}")]
+        //public StatusDTO GetStatusById(int statusId)
+        //{
+        //    return _directoryService.GetStatusById(statusId);
+        //}
+
+        //[HttpGet("GetAllStatuses")]
+        //public List<StatusDTO> GetAllStatuses()
+        //{
+        //    return _directoryService.GetAllStatuses();
+        //}
     }
 }

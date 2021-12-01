507bcddc96e6a82e012e2c51d006a0afef733db0
Nikolay Kusakin <NikolayKusakin@yandex.by>
2021-12-01 20:00:12 +0300

    Added dto

---
+++ BLL/DTO/CandidateForTechDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class CandidateForTechDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool? IsInterviewedByHR { get; set; }
+        public bool? IsInterviewedByTech { get; set; }
+    }
+}

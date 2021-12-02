31997f77a8d7d49063487d25b1e669474efd3acd
JOKER <sasha.golubovich@tut.by>
2021-12-02 13:47:47 +0300

    Delete old candidates; add property "CountryID" to Candiddate.cs

---
+++ DAL/Migrations/20211202103245_DeleteCandidatesFromDB.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class DeleteCandidatesFromDB : Migration
+    {
+        private string seedScriptForDelettingCandidatesFromDB = @"
+SET IDENTITY_INSERT Candidates ON;
+DELETE From Candidates
+SET IDENTITY_INSERT Candidates OFF;
+";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(seedScriptForDelettingCandidatesFromDB);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

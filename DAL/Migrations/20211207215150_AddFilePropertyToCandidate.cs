2b0b5582d269c1aae18722ff0f9c4812fe9998fc
JOKER <sasha.golubovich@tut.by>
2021-12-08 00:57:39 +0300

    Add migration for Files

---
+++ DAL/Migrations/20211207215150_AddFilePropertyToCandidate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddFilePropertyToCandidate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileUrl",
+                table: "Candidates",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileUrl",
+                table: "Candidates");
+        }
+    }
+}
